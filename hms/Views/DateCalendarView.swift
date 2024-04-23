afa46aef6c5ce768da461ad727c16d34fc03cb24
aahlaad26 <aahlaad26092002@gmail.com>
2024-04-23 23:31:13 +0530

    Added slots in ui

--- hms/Views/DateCalendarView.swift
+++ hms/Views/DateCalendarView.swift
@@ -8,7 +8,7 @@ class Booking: ObservableObject {
 let booking: Booking = Booking()
 
 struct DateCalendarView: View {
-    @State private var hello : String = "hello"
+    @State private var hello : String = "Mon"
     @State private var presentAppointments = false
     @State private var dayColor: Color = Color(red: 161/255, green: 168/255, blue: 176/255)
     @State private var dateColor: Color = Color(red: 16/255, green: 22/255, blue: 35/255)
@@ -55,12 +55,12 @@ struct DateCalendarView: View {
                 HStack {
                     Text(getMonthAndYear(date: selectedDate))
                         .font(.headline)
-                        .foregroundColor(.blue)
+                        .foregroundColor(.black)
                     Button(action: {
                         self.isDropdownExpanded.toggle()
                     }) {
                         Image(systemName: "chevron.down")
-                            .foregroundColor(.blue)
+                            .foregroundColor(.black)
                     }
                     Spacer()
                 }
@@ -96,7 +96,7 @@ struct DateCalendarView: View {
                                 .background(
                                     RoundedRectangle(cornerRadius: 15)
                                         .frame(width: 64, height: 80)
-                                        .foregroundColor(isSelected ? .blue : .white)
+                                        .foregroundColor(isSelected ? Color(red: 0.24, green: 0.24, blue: 0.26).opacity(0.36) : .white)
                                         .padding(.leading)
                                 )
                                 .padding()
@@ -133,6 +133,11 @@ struct DateCalendarView: View {
                         .padding()
                 }
             }
+        }.background(
+            LinearGradient(gradient: Gradient(colors: [Color(red: 0, green: 0.60, blue: 0.87), Color(red: 0.56, green: 0.87, blue: 0.97)]), startPoint: .top, endPoint: .bottom)
+          )
+        .onAppear(){
+            self.hello = getDay(date: Date())
         }
     }
 }
@@ -148,7 +153,87 @@ struct AppointmentView : View {
     var body : some View{
         
         VStack{
-            Text("\(temp)")
+            ForEach(0..<3){i in
+                if(i == 1){
+                    HStack{
+                        ZStack() {
+                          Rectangle()
+                            .foregroundColor(.clear)
+                            .frame(width: 86.78, height: 96)
+                            .background(.white)
+                            .cornerRadius(20)
+                            .offset(x: 0, y: 0)
+                            Text("\(temp)")
+                            .font(Font.custom("SF Pro Display", size: 18).weight(.medium))
+                            .foregroundColor(.black)
+                            .offset(x: 3.11, y: -2.50)
+                        }
+                        .frame(width: 86.78, height: 96)
+                        ZStack() {
+                          Rectangle()
+                            .foregroundColor(.clear)
+                            .frame(width: 249.62, height: 96)
+                            .background(.white)
+                            .cornerRadius(20)
+                            .offset(x: 0, y: 0)
+                          Text("11:00-12:00 PM")
+                            .font(Font.custom("SF Pro Display", size: 16).weight(.semibold))
+                            .tracking(0.16)
+                            .lineSpacing(21.60)
+                            .foregroundColor(.black)
+                            .offset(x: -43.41, y: -19)
+                          Text("3 Patients")
+                            .font(Font.custom("SF Pro Display", size: 12))
+                            .tracking(0.12)
+                            .lineSpacing(16.20)
+                            .foregroundColor(.black)
+                            .offset(x: -74.93, y: 3)
+                        }
+                        .frame(width: 249.62, height: 96)
+                    }
+                    
+                }
+                else{
+                    HStack{
+                        ZStack() {
+                          Rectangle()
+                            .foregroundColor(.clear)
+                            .frame(width: 86.78, height: 96)
+                            .background(Color(red: 0.24, green: 0.24, blue: 0.26).opacity(0.36))
+                            .cornerRadius(20)
+                            .offset(x: 0, y: 0)
+                          Text("\(temp)")
+                            .font(Font.custom("SF Pro Display", size: 18).weight(.medium))
+                            .foregroundColor(.white)
+                            .offset(x: 3.11, y: -2.50)
+                        }
+                        .frame(width: 86.78, height: 96)
+                        ZStack() {
+                          Rectangle()
+                            .foregroundColor(.clear)
+                            .frame(width: 249.62, height: 96)
+                            .background(Color(red: 0.24, green: 0.24, blue: 0.26).opacity(0.36))
+                            .cornerRadius(20)
+                            .offset(x: 0, y: 0)
+                          Text("11:00-12:00 PM")
+                            .font(Font.custom("SF Pro Display", size: 16).weight(.semibold))
+                            .tracking(0.16)
+                            .lineSpacing(21.60)
+                            .foregroundColor(.white)
+                            .offset(x: -43.41, y: -19)
+                          Text("3 Patients")
+                            .font(Font.custom("SF Pro Display", size: 12))
+                            .tracking(0.12)
+                            .lineSpacing(16.20)
+                            .foregroundColor(.white)
+                            .offset(x: -74.93, y: 3)
+                        }
+                        .frame(width: 249.62, height: 96)
+                    }
+                    
+                }
+            }
+            
         }
     }
 }
