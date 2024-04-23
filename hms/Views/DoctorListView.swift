bbd65f164ff4c78b1f13c847a3a8e8202e09eccd
ishan9678 <is9678@srmist.edu.in>
2024-04-23 14:00:57 +0530

    patient home revamp

---
+++ hms/Views/DoctorListView.swift
@@ -0,0 +1,18 @@
+//
+//  DoctorListView.swift
+//  hms
+//
+//  Created by Ishan on 23/04/24.
+//
+
+import SwiftUI
+
+struct DoctorListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DoctorListView()
+}
---
+++ hms/Views/PatientHomeView.swift
@@ -0,0 +1,18 @@
+//
+//  PatientHomeView.swift
+//  hms
+//
+//  Created by Ishan on 23/04/24.
+//
+
+import SwiftUI
+
+struct PatientHomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PatientHomeView()
+}
