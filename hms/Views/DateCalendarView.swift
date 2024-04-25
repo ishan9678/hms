c80df799035ed05646c0619fee8b9f72dd87c4c7
aahlaad26 <aahlaad26092002@gmail.com>
2024-04-25 12:03:33 +0530

    Date Calender slots

--- hms/Views/DateCalendarView.swift
+++ hms/Views/DateCalendarView.swift
@@ -49,95 +49,100 @@ struct DateCalendarView: View {
     @ObservedObject var indexDate = booking
     
     var body: some View {
-        ZStack {
-            VStack {
-                // Dropdown list to select month and year
-                HStack {
-                    Text(getMonthAndYear(date: selectedDate))
-                        .font(.headline)
-                        .foregroundColor(.black)
-                    Button(action: {
-                        self.isDropdownExpanded.toggle()
-                    }) {
-                        Image(systemName: "chevron.down")
-                            .foregroundColor(.black)
-                    }
-                    Spacer()
-                }
-                .padding([.horizontal,.top])
-                
-                .cornerRadius(10)
-                
-                // Your existing scroll view for the week
-               
-                ScrollView(.horizontal, showsIndicators: false) {
-                    HStack {
-                        ForEach(0..<7) { i in
-                            let date = dateGetter(index: i)
-                            let isSelected = Calendar.current.isDate(date, inSameDayAs: selectedDate)
-                            
-                            Button(action: {withAnimation {
-                                indexDate.index = i
-                                selectedDate = date
-                                hello = getDay(date: date)
+        NavigationStack{
+            ScrollView{
+                ZStack {
+                    VStack {
+                        // Dropdown list to select month and year
+                        HStack {
+                            Text(getMonthAndYear(date: selectedDate))
+                                .font(.headline)
+                                .foregroundColor(.black)
+                            Button(action: {
+                                self.isDropdownExpanded.toggle()
+                            }) {
+                                Image(systemName: "chevron.down")
+                                    .foregroundColor(.black)
                             }
-                            print(date)}) {
-                                VStack {
-                                    Text((getDay(date: date)))
-                                        .font(Font.custom("SF Pro Display Regular", size: 16))
-                                        .foregroundColor(isSelected ? .white : dayColor)
+                            Spacer()
+                        }
+                        .padding([.horizontal,.top])
+                        
+                        .cornerRadius(10)
+                        
+                        // Your existing scroll view for the week
+                       
+                        ScrollView(.horizontal, showsIndicators: false) {
+                            HStack {
+                                ForEach(0..<7) { i in
+                                    let date = dateGetter(index: i)
+                                    let isSelected = Calendar.current.isDate(date, inSameDayAs: selectedDate)
                                     
-                                    Text((getDate(date: date)))
-                                        .font(Font.custom("SF Pro Display ", size: 18))
-                                        .foregroundColor(isSelected ? .white : dateColor)
-                                }
-                                .frame(alignment: .center)
-                                .padding(.leading)
-                                .background(
-                                    RoundedRectangle(cornerRadius: 15)
-                                        .frame(width: 64, height: 80)
-                                        .foregroundColor(isSelected ? Color(red: 0.24, green: 0.24, blue: 0.26).opacity(0.36) : .white)
+                                    Button(action: {withAnimation {
+                                        indexDate.index = i
+                                        selectedDate = date
+                                        hello = getDay(date: date)
+                                    }
+                                    print(date)}) {
+                                        VStack {
+                                            Text((getDay(date: date)))
+                                                .font(Font.custom("SF Pro Display Regular", size: 16))
+                                                .foregroundColor(isSelected ? .white : dayColor)
+                                            
+                                            Text((getDate(date: date)))
+                                                .font(Font.custom("SF Pro Display ", size: 18))
+                                                .foregroundColor(isSelected ? .white : dateColor)
+                                        }
+                                        .frame(alignment: .center)
                                         .padding(.leading)
-                                )
-                                .padding()
+                                        .background(
+                                            RoundedRectangle(cornerRadius: 15)
+                                                .frame(width: 64, height: 80)
+                                                .foregroundColor(isSelected ? Color(red: 0.24, green: 0.24, blue: 0.26).opacity(0.36) : .white)
+                                                .padding(.leading)
+                                        )
+                                        .padding()
+                                    }
+                                }
                             }
+                            .frame(height: 85)
+                            .padding(.bottom)
+                            .padding(.trailing, 16)
+                            
+                        }
+                        
+                        AppointmentView(temp: hello)
+                        Spacer()
+                    }
+                    
+                    
+                    if isDropdownExpanded {
+                        Color.black.opacity(0.5)
+                            .edgesIgnoringSafeArea(.all)
+                            .onTapGesture {
+                                self.isDropdownExpanded.toggle()
+                            }
+                        
+                        // Modal containing DatePicker
+                        VStack {
+                            Spacer()
+                            DatePicker("", selection: $selectedDate, displayedComponents: [.date])
+                                .datePickerStyle(GraphicalDatePickerStyle())
+                                .padding()
+                                .background(Color.white)
+                                .cornerRadius(10)
+                                .shadow(radius: 5)
+                                .padding()
                         }
                     }
-                    .frame(height: 85)
-                    .padding(.bottom)
-                    .padding(.trailing, 16)
-                    
                 }
-                
-                AppointmentView(temp: hello)
-                Spacer()
-            }
-            
-            
-            if isDropdownExpanded {
-                Color.black.opacity(0.5)
-                    .edgesIgnoringSafeArea(.all)
-                    .onTapGesture {
-                        self.isDropdownExpanded.toggle()
-                    }
-                
-                // Modal containing DatePicker
-                VStack {
-                    Spacer()
-                    DatePicker("", selection: $selectedDate, displayedComponents: [.date])
-                        .datePickerStyle(GraphicalDatePickerStyle())
-                        .padding()
-                        .background(Color.white)
-                        .cornerRadius(10)
-                        .shadow(radius: 5)
-                        .padding()
+                .onAppear(){
+                    self.hello = getDay(date: Date())
                 }
             }
-        }.background(
-            LinearGradient(gradient: Gradient(colors: [Color(red: 0, green: 0.60, blue: 0.87), Color(red: 0.56, green: 0.87, blue: 0.97)]), startPoint: .top, endPoint: .bottom)
-          )
-        .onAppear(){
-            self.hello = getDay(date: Date())
+            .background(
+                LinearGradient(gradient: Gradient(colors: [Color(red: 0, green: 0.60, blue: 0.87), Color(red: 0.56, green: 0.87, blue: 0.97)]), startPoint: .top, endPoint: .bottom)
+              )
         }
     }
 }
@@ -153,76 +158,144 @@ struct AppointmentView : View {
     var body : some View{
         
         VStack{
-            ForEach(0..<3){i in
+            ForEach(0..<4){i in
                 if(i == 1){
-                    HStack{
-                        ZStack() {
-                          Rectangle()
-                            .foregroundColor(.clear)
-                            .frame(width: 86.78, height: 96)
-                            .background(.white)
-                            .cornerRadius(20)
-                            Text("\(temp)")
-                            .font(Font.custom("SF Pro Display", size: 18).weight(.medium))
-                            .foregroundColor(.black)
-                        }
-                        .frame(width: 86.78, height: 96)
-                        ZStack() {
-                          Rectangle()
-                            .foregroundColor(.clear)
-                            .frame(width: 249.62, height: 96)
-                            .background(.white)
-                            .cornerRadius(20)
-                            VStack(alignment: .leading){
-                                Text("11:00-12:00 PM")
-                                  .font(Font.custom("SF Pro Display", size: 16).weight(.semibold))
-                                  .foregroundColor(.black)
-                                  .padding(.bottom)
-                                Text("3 Patients")
-                                  .font(Font.custom("SF Pro Display", size: 12))
-                                  .foregroundColor(.black)
+                    DisclosureGroup(
+                        content: { VStack{
+                            ForEach(0..<3){
+                                j in 
+                                HStack() {
+                                    VStack(alignment: .leading){
+                                        Text("26year old")
+                                          .font(Font.custom("SF Pro Display", size: 12))
+                                          .tracking(0.12)
+                                          .lineSpacing(16.20)
+                                          .foregroundColor(.black)
+                                        Text("ISHAN REDDY")
+                                          .font(Font.custom("SF Pro Display", size: 16).weight(.semibold))
+                                          .tracking(0.16)
+                                          .lineSpacing(21.60)
+                                          .foregroundColor(.black)
+
+                                    }
+                                    .padding(.leading)
+                                    
+                                  Spacer()
+                                    Image(systemName: "arrow.forwardarrow.forward")
+                                }
+                                .frame(width: 218, height: 84)
+                                .background(.white)
+                                .clipShape(RoundedRectangle(cornerRadius: 20))
+                                .padding([.horizontal,.vertical],5)
                             }
-                            .offset(x:-30)
-                        }
-                        .frame(width: 249.62, height: 96)
-                    }
+                            
+                        }.background(Color(red: 0.24, green: 0.24, blue: 0.26).opacity(0.36))
+                                .clipShape(RoundedRectangle(cornerRadius: 20))
+                                .padding(.leading,100)
+                        },
+                        label: { HStack{
+                            ZStack() {
+                              Rectangle()
+                                .foregroundColor(.clear)
+                                .frame(width: 86.78, height: 96)
+                                .background(.white)
+                                .cornerRadius(20)
+                                Text("\(temp)")
+                                .font(Font.custom("SF Pro Display", size: 18).weight(.medium))
+                                .foregroundColor(.black)
+                            }
+                            .frame(width: 86.78, height: 96)
+                            ZStack() {
+                              Rectangle()
+                                .foregroundColor(.clear)
+                                .frame(width: 249.62, height: 96)
+                                .background(.white)
+                                .cornerRadius(20)
+                                VStack(alignment: .leading){
+                                    Text("11:00-12:00 PM")
+                                      .font(Font.custom("SF Pro Display", size: 16).weight(.semibold))
+                                      .foregroundColor(.black)
+                                      .padding(.bottom)
+                                    Text("3 Patients")
+                                      .font(Font.custom("SF Pro Display", size: 12))
+                                      .foregroundColor(.black)
+                                }
+                                .offset(x:-30)
+                            }
+                            .frame(width: 249.62, height: 96)
+                        } }
+                    ).padding(.horizontal)
                     
                 }
                 else{
-                    HStack{
-                        ZStack() {
-                          Rectangle()
-                            .foregroundColor(.clear)
-                            .frame(width: 86.78, height: 96)
-                            .background(Color(red: 0.24, green: 0.24, blue: 0.26).opacity(0.36))
-                            .cornerRadius(20)
-                          Text("\(temp)")
-                            .font(Font.custom("SF Pro Display", size: 18).weight(.medium))
-                            .foregroundColor(.white)
-                            
-                        }
-                        .frame(width: 86.78, height: 96)
-                        ZStack() {
-                          Rectangle()
-                            .foregroundColor(.clear)
-                            .frame(width: 249.62, height: 96)
-                            .background(Color(red: 0.24, green: 0.24, blue: 0.26).opacity(0.36))
-                            .cornerRadius(20)
-                            .offset(x: 0, y: 0)
-                            VStack(alignment: .leading){
-                                Text("11:00-12:00 PM")
-                                  .font(Font.custom("SF Pro Display", size: 16).weight(.semibold))
-                                  .foregroundColor(.white)
-                                  .padding(.bottom)
-                                Text("3 Patients")
-                                  .font(Font.custom("SF Pro Display", size: 12))
-                                  .foregroundColor(.white)
-                                 
+                    DisclosureGroup(
+                        content: { VStack{
+                            ForEach(0..<3){
+                                j in
+                                HStack() {
+                                    VStack(alignment: .leading){
+                                        Text("26year old")
+                                          .font(Font.custom("SF Pro Display", size: 12))
+                                          .tracking(0.12)
+                                          .lineSpacing(16.20)
+                                          .foregroundColor(.black)
+                                        Text("ISHAN REDDY")
+                                          .font(Font.custom("SF Pro Display", size: 16).weight(.semibold))
+                                          .tracking(0.16)
+                                          .lineSpacing(21.60)
+                                          .foregroundColor(.black)
+
+                                    }
+                                    .padding(.leading)
+                                    
+                                  Spacer()
+                                    Image(systemName: "arrow.forwardarrow.forward")
+                                }
+                                .frame(width: 218, height: 84)
+                                .background(.white)
+                                .clipShape(RoundedRectangle(cornerRadius: 20))
+                                .padding([.horizontal,.vertical],5)
                             }
-                            .offset(x:-30)
-                        }
-                        .frame(width: 249.62, height: 96)
-                    }
+                            
+                        }.background(Color(red: 0.24, green: 0.24, blue: 0.26).opacity(0.36))
+                                .clipShape(RoundedRectangle(cornerRadius: 20))
+                                .padding(.leading,100)
+                        },
+                        label: { HStack{
+                            ZStack() {
+                              Rectangle()
+                                .foregroundColor(.clear)
+                                .frame(width: 86.78, height: 96)
+                                .background(Color(red: 0.24, green: 0.24, blue: 0.26).opacity(0.36))
+                                .cornerRadius(20)
+                              Text("\(temp)")
+                                .font(Font.custom("SF Pro Display", size: 18).weight(.medium))
+                                .foregroundColor(.white)
+                                
+                            }
+                            .frame(width: 86.78, height: 96)
+                            ZStack() {
+                              Rectangle()
+                                .foregroundColor(.clear)
+                                .frame(width: 249.62, height: 96)
+                                .background(Color(red: 0.24, green: 0.24, blue: 0.26).opacity(0.36))
+                                .cornerRadius(20)
+                                .offset(x: 0, y: 0)
+                                VStack(alignment: .leading){
+                                    Text("11:00-12:00 PM")
+                                      .font(Font.custom("SF Pro Display", size: 16).weight(.semibold))
+                                      .foregroundColor(.white)
+                                      .padding(.bottom)
+                                    Text("3 Patients")
+                                      .font(Font.custom("SF Pro Display", size: 12))
+                                      .foregroundColor(.white)
+                                     
+                                }
+                                .offset(x:-30)
+                            }
+                            .frame(width: 249.62, height: 96)
+                        } }
+                    ).padding(.horizontal)
                     
                 }
             }
